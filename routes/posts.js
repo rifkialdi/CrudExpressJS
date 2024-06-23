fd4148f6583a8ec86e5ce68f44f7c05f2e603bdb
RifkiAldiansyah <rifki@goodeva.co.id>
2024-06-23 17:07:00 +0700

    update data

--- routes/posts.js
+++ routes/posts.js
@@ -31,7 +31,7 @@ router.get('/', function (req, res) {
 /**
  * STORE POST
  */
- router.post('/store', [
+router.post('/store', [
 
     //validation
     body('title').notEmpty(),
@@ -75,7 +75,7 @@ router.get('/', function (req, res) {
 /**
  * SHOW POST
  */
- router.get('/(:id)', function (req, res) {
+router.get('/(:id)', function (req, res) {
 
     let id = req.params.id;
 
@@ -104,6 +104,52 @@ router.get('/', function (req, res) {
             })
         }
     })
-})
+});
+
+/**
+ * UPDATE POST
+ */
+router.patch('/update/:id', [
+
+    //validation
+    body('title').notEmpty(),
+    body('content').notEmpty()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors.array()
+        });
+    }
+
+    //id post
+    let id = req.params.id;
+
+    //data post
+    let formData = {
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    // update query
+    connection.query(`UPDATE posts SET ? WHERE id = ${id}`, formData, function (err, rows) {
+        //if(err) throw err
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error',
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'Update Data Successfully!'
+            })
+        }
+    })
+
+});
 
 module.exports = router;
